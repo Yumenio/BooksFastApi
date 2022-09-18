c6225f548560fb6630f8e63c703bec7aa1db7246
Yumenio <alejandroandy447@gmail.com>
2022-09-18 16:39:18 -0400

    create operation done

--- src/db.py
+++ src/db.py
@@ -2,9 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float, Date
 
-engine = create_engine('sqlite:///books.db')
+engine = create_engine('sqlite:///booksdb.db')
 Base = declarative_base()
-Base.metadata.create_all(engine)
 
 
 
@@ -12,8 +11,8 @@ class Book(Base):
     # name, year, author, price and availability(amount)
     __tablename__ = 'Books'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False)
-    year_published = Column(Date)
-    author = Column(String(20), nullable=False)
-    price = Column(Float, nullable=False)
-    availability = Column(Integer, nullable=False)
+    name = Column(String(50))
+    # year_published = Column(Date)
+    # author = Column(String(20), nullable=False)
+    # price = Column(Float, nullable=False)
+    # availability = Column(Integer, nullable=False)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
-from fastapi import status
+from fastapi import FastAPI, status, HTTPException
+from src.schemas import BookRequest
 
-from .db import Base, engine
+from .db import Base, engine, Book
 
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+
+Base.metadata.create_all(engine)
 
 app = FastAPI()
 
@@ -12,12 +16,34 @@ def root():
     return {"msg" : "Hello World"}
 
 @app.post("/books", status_code=status.HTTP_201_CREATED)
-def create_book():
-    return "create"
+def create_book(book: BookRequest):
+    session = Session(bind=engine, expire_on_commit=False)
+
+    new_book = Book(
+        name = book.name
+    )
+    session.add(new_book)
+    session.commit()
+
+    id = new_book.id
+
+    session.close()
+
+
+    return "created new book"
 
 @app.get("/books/{id}")
 def read_book(id):
-    return "read"
+    session = Session(bind=engine, expire_on_commit=False)
+
+    book = session.query(Book).get(id)
+    session.close()
+    
+    if not book:
+        raise HTTPException(status_code=404, detail='Book not found')
+    
+    return book
+    # return {"book_id":book.id, "book_name": book.name}
 
 @app.put("/books/{id}")
 def update_book(id):
